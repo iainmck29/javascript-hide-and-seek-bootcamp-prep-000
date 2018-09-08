c5fa9af09d3855f02b3132581c9d43f126d9798b
Iain McKenzie <iainmck29@hotmail.com>
2018-09-08 17:06:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,4 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
 
-  for (let i = 0; i <lis.length; i++) {
-    const lis = document.querySelector('ul.ranked-list li');
-    lis[i].innerHTML = (parseInt(lis[i].innerHTML)+n)
-  }
 }
