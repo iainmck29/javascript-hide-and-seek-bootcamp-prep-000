7735bd0f82d15287e7992f1d369283cf9ecb253a
Iain McKenzie <iainmck29@hotmail.com>
2018-09-08 17:02:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function getFirstSelector(selector) {
+return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelector('ul.ranked-list li');
+  for (let i = 0; i <lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML)+n)
+  }
+}
